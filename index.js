5517da151fc90554aa5d28a90a1c5a299c1f04c1
Paul M Shumate <+>
2024-01-31 13:21:32 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
-const titleCased = () => {
-  return tutorials
+function titleCase(str) {
+  const words = str.split(' ');
+  const titleCasedWords = words.map(word => capitalizeFirstLetter(word));
+  return titleCasedWords.join(' ');
+}
+
+function titleCased() {
+  const tutorials = [
+    "what does the this keyword mean?",
+    "What is the Constructor OO pattern?",
+    "implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What is NaN and how Can we Check for it",
+    "What is the difference between stopPropagation and preventDefault?",
+    "Immutable State and Pure Functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?",
+  ];
+
+  return tutorials.map(titleCase);
 }
